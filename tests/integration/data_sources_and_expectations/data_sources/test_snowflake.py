1295a18e54c493523d2443de00ec20a9eb13c398
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-11-27 08:15:37 +0100

    test snowflake numeric

--- tests/integration/data_sources_and_expectations/data_sources/test_snowflake.py
+++ tests/integration/data_sources_and_expectations/data_sources/test_snowflake.py
@@ -1,18 +1,14 @@
-from typing import Generator
-
 import pandas as pd
 import pytest
-from sqlalchemy.orm import MappedColumn
 
 from great_expectations.compatibility import snowflake
 from great_expectations.compatibility.sqlalchemy import sqltypes
-from great_expectations.datasource.fluent.interfaces import Batch
 from great_expectations.expectations import ExpectColumnSumToBeBetween
 from tests.integration.test_utils.data_source_config import SnowflakeDatasourceTestConfig
 from tests.integration.test_utils.data_source_config.snowflake import SnowflakeBatchTestSetup
 
 
-class TestSnowflakeTypes:
+class TestDataTypes:
     """This set of tests ensures that we can run expectations against every data
     type supported by Snowflake.
 
@@ -23,7 +19,7 @@ class TestSnowflakeTypes:
 
     @pytest.mark.snowflake
     @pytest.mark.parametrize(
-        "data_type",
+        "column_type",
         [
             snowflake.NUMBER,
             sqltypes.NUMERIC,
@@ -36,18 +32,20 @@ class TestSnowflakeTypes:
             sqltypes.REAL,
         ],
     )
-    def test_numeric(self, data_type):
-        batch = self.setup_batch(
+    def test_numeric(self, column_type):
+        batch_setup = SnowflakeBatchTestSetup(
+            config=SnowflakeDatasourceTestConfig(column_types={self.COLUMN: column_type}),
             data=pd.DataFrame({self.COLUMN: [1, 2, 3, 4]}),
-            column_type=data_type,
+            extra_data={},
         )
-        result = batch.validate(
-            expect=ExpectColumnSumToBeBetween(
-                column=self.COLUMN,
-                min_value=9,
-                max_value=11,
+        with batch_setup.batch_test_context() as batch:
+            result = batch.validate(
+                expect=ExpectColumnSumToBeBetween(
+                    column=self.COLUMN,
+                    min_value=9,
+                    max_value=11,
+                )
             )
-        )
         assert result.success
 
     def test_string(self): ...
@@ -61,27 +59,3 @@ class TestSnowflakeTypes:
     def test_geospatial(self): ...
 
     def test_vector(self): ...
-
-    def setup_batch(self, data: pd.DataFrame, column_type: MappedColumn) -> Batch:
-        # generate batch inside a closure so we can also clean it up on test exit
-        def _batch_generator(
-            data: pd.DataFrame, column_type: MappedColumn
-        ) -> Generator[Batch, None, None]:
-            batch_setup = SnowflakeBatchTestSetup(
-                config=SnowflakeDatasourceTestConfig(column_types={self.COLUMN: column_type}),
-                data=data,
-                extra_data={},
-            )
-            batch_setup.setup()
-
-            yield batch_setup.make_batch()
-
-            batch_setup.teardown()
-
-        return next(_batch_generator(data=data, column_type=column_type))
-
-
-class TestPostgreSQLTypes: ...
-
-
-class TestDatabricksSQLTypes: ...
