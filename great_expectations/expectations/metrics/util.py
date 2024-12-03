aeed373c9c4e11f7140ed0411a13c363ea450800
Thu Pham <thu.pham@greatexpectations.io>
2024-12-02 16:13:41 -0800

    Lint

--- great_expectations/expectations/metrics/util.py
+++ great_expectations/expectations/metrics/util.py
@@ -406,7 +406,7 @@ class CaseInsensitiveNameDict(UserDict):
         return item
 
 
-def get_sqlalchemy_column_metadata(  # noqa: C901
+def get_sqlalchemy_column_metadata(  # noqa: C901, PLR0912
     execution_engine: SqlAlchemyExecutionEngine,
     table_selectable: sqlalchemy.Select,
     schema_name: Optional[str] = None,
