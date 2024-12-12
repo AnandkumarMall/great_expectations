0b7cba6408eb0e23e533d69b67b15dba3f1e7620
Chetan Kini <chetan@superconductive.com>
2024-12-12 13:48:51 -0500

    fix test

--- tests/integration/data_sources_and_expectations/test_canonical_expectations.py
+++ tests/integration/data_sources_and_expectations/test_canonical_expectations.py
@@ -158,7 +158,7 @@ def test_expect_column_mean_to_be_between(batch_for_datasource):
 
 
 @parameterize_batch_for_data_sources(
-    data_source_configs=[SparkFilesystemCsvDatasourceTestConfig()],
+    data_source_configs=[PostgreSQLDatasourceTestConfig()],
     data=pd.DataFrame(
         {
             "col1": [1, 2, 3, 4, 5],
@@ -171,18 +171,27 @@ def test_missing_condition_parser_causes_entire_suite_to_fail(batch_for_datasour
     suite = ExpectationSuite(
         name="faulty",
         expectations=[
-            gxe.ExpectColumnValuesToNotBeNull(column="col1", result_format="COMPLETE"),
+            gxe.ExpectColumnValuesToNotBeNull(column="col1"),
+            gxe.ExpectColumnValuesToNotBeNull(column="col2"),
             gxe.ExpectColumnValuesToBeInSet(
                 column="col2",
                 value_set=["A", "B", "C"],
                 row_condition="col3 IS NOT NULL",
                 mostly=0.665,
-                # condition_parser='spark',
-                result_format="COMPLETE",
+                condition_parser=None,  # Should be specified as 'great_expectations'
             ),
         ],
     )
 
     result = batch_for_datasource.validate(suite)
     assert not result.success
-    assert all(res.success is False and res.exception_info for res in result.results)
+    # Resolution of the 'table.row_count' metric (which is used by all expectations above) fails
+    # because the condition_parser is inaccurate.
+    # BUG - The error message should not permeates across the suite due to the shared metric
+    # dependency.
+    assert all(
+        res.success is False
+        and "SqlAlchemyExecutionEngine only supports the great_expectations condition_parser."
+        in str(res.exception_info)
+        for res in result.results
+    )
