7b166f045d1240e1910763a9356bba24f3f679b3
Chetan Kini <chetan@superconductive.com>
2025-01-08 19:17:49 -0500

    update requirements files

--- tasks.py
+++ tasks.py
@@ -847,6 +847,7 @@ MARKER_DEPENDENCY_MAP: Final[Mapping[str, TestDependencies]] = {
             "reqs/requirements-dev-test.txt",
             "reqs/requirements-dev-azure.txt",
             "reqs/requirements-dev-bigquery.txt",
+            "reqs/requirements-dev-cloud.txt",
             "reqs/requirements-dev-redshift.txt",
             "reqs/requirements-dev-snowflake.txt",
             # "Deprecated API features detected" warning/error for test_docs[split_data_on_whole_table_bigquery] when pandas>=2.0  # noqa: E501
