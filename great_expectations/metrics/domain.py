632ce0c7832dd34561b7713845bdb3dee240beae
Farmer <nathan@greatexpectations.io>
2025-02-08 23:13:54 -0700

    Add domain tests

--- great_expectations/metrics/domain.py
+++ great_expectations/metrics/domain.py
@@ -6,17 +6,32 @@ from great_expectations.compatibility.pydantic import BaseModel, Field, StrictSt
 NonEmptyString = Annotated[StrictStr, Field(min_length=1)]
 
 
-class Domain(ABC, BaseModel):
-    """The abstract base class for defining all subsets of data within a dataset."""
+class AbstractClassInstantiationError(TypeError):
+    def __init__(self, class_name: str) -> None:
+        super().__init__(f"Cannot instantiate abstract class `{class_name}`.")
+
+
+class Domain(BaseModel, ABC):
+    """The abstract base class for defining all types of domains over which metrics are computed."""
 
     batch_id: NonEmptyString
 
+    def __new__(cls, *args, **kwargs):
+        if cls is Domain:
+            raise AbstractClassInstantiationError(cls.__name__)
+        return super().__new__(cls)
+
 
 class Map(Domain, ABC):
-    """The generic type for metrics that compute row-level calculations."""
+    """The abstract base class for metric domain types that compute row-level calculations."""
 
     table: NonEmptyString
     row_condition: Optional[StrictStr] = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls is Map:
+            raise AbstractClassInstantiationError(cls.__name__)
+        return super().__new__(cls)
 
 
 class ColumnMap(Map):
---
+++ tests/metrics/test_domain.py
@@ -0,0 +1,42 @@
+from uuid import uuid4
+
+import pytest
+
+from great_expectations.compatibility.pydantic import ValidationError, errors
+from great_expectations.metrics.domain import ColumnMap, Domain, Map
+
+BATCH_ID = str(uuid4())
+TABLE = "my_table"
+COLUMN = "my_column"
+
+
+class TestAbstractClasses:
+    def test_domain_instantiation_raises(self):
+        with pytest.raises(TypeError):
+            Domain(batch_id=BATCH_ID)
+
+    def test_map_instantiation_raises(self):
+        with pytest.raises(TypeError):
+            Map(batch_id=BATCH_ID, table=TABLE)
+
+
+class TestColumnMap:
+    def test_column_map_instantiation_success(self):
+        ColumnMap(batch_id=BATCH_ID, table=TABLE, column=COLUMN)
+
+    @pytest.mark.parametrize(
+        "kwargs",
+        [
+            {"batch_id": "", "table": TABLE, "column": COLUMN},
+            {"batch_id": BATCH_ID, "table": "", "column": COLUMN},
+            {"batch_id": BATCH_ID, "table": TABLE, "column": ""},
+        ],
+    )
+    def test_column_map_arguments_non_empty_string_raises(self, kwargs: dict):
+        with pytest.raises(ValidationError) as e:
+            ColumnMap(**kwargs)
+        all_errors = e.value.raw_errors
+        assert any(
+            True if isinstance(error.exc, errors.AnyStrMinLengthError) else False
+            for error in all_errors
+        )
