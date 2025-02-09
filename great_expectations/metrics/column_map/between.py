3c7f4dea8e55e390dc7d685768ee89a08d1a4770
Farmer <nathan@greatexpectations.io>
2025-02-08 22:19:40 -0700

    Add example column map metric

---
+++ great_expectations/metrics/column_map/between.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from great_expectations.core.types import Comparable
+from great_expectations.metrics import Metric
+from great_expectations.metrics.domain import ColumnMap
+
+
+class Between(Metric[ColumnMap]):
+    min_value: Optional[Comparable] = None
+    max_value: Optional[Comparable] = None
+    strict_min: bool = False
+    strict_max: bool = False
