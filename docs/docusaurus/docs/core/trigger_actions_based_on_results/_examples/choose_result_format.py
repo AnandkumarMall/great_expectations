a706ee97e8cb1654089385cc5111630dd6db3797
Galina Yermicheva <galina.yermicheva@greatexpectations.io>
2026-01-08 12:53:08 -0500

    Persist the checkpoint to the cloud

--- docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/choose_result_format.py
+++ docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/choose_result_format.py
@@ -55,7 +55,7 @@ def set_up_context_for_example(context):
     checkpoint_name = "my_checkpoint"
     validation_definitions = [validation_definition]
     action_list = []
-    context.checkpoints.add(
+    checkpoint = context.checkpoints.add(
         gx.Checkpoint(
             name=checkpoint_name,
             validation_definitions=validation_definitions,
@@ -63,6 +63,7 @@ def set_up_context_for_example(context):
             result_format={"result_format": "COMPLETE"},
         )
     )
+    checkpoint.save()
 
 
 # EXAMPLE SCRIPT STARTS HERE:
